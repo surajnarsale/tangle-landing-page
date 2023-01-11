5e6c89456ea8ffff33409852e1b52ad40a312235
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-01-11 22:41:36 +0530

    feat: add env for api url

--- config/dev.ts
+++ config/dev.ts
@@ -2,5 +2,5 @@ import type { Config } from '@/config/index'
 
 export const devConfig: Config = {
   env: 'dev',
-  graphqlBaseUrl: 'https://api-dev.dontiaalliance.com/graphql',
+  graphqlBaseUrl: process.env.WORDPRESS_API_URL!,
 }
