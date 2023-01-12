409dddba622ae91dda90c201275662df3be95291
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-01-12 19:22:12 +0530

    fix: fix tailwind and extend classes

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -12,11 +12,11 @@ function MobileNavigation() {
     <Popover>
       {({ open, close }) => (
         <>
-          <Popover.Button className="relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none">
+          <Popover.Button className="relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none ">
             <span className="sr-only">Toggle Navigation</span>
             <svg
               aria-hidden="true"
-              className="stroke-slate-700 h-3.5 w-3.5 overflow-visible"
+              className="h-3.5 w-3.5 overflow-visible stroke-slate-700"
               fill="none"
               strokeWidth={2}
               strokeLinecap="round"
@@ -45,7 +45,7 @@ function MobileNavigation() {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Popover.Overlay className="bg-slate-300/50 fixed inset-0" />
+              <Popover.Overlay className="fixed inset-0 bg-slate-300/50" />
             </Transition.Child>
             <Transition.Child
               as={Fragment}
@@ -58,7 +58,7 @@ function MobileNavigation() {
             >
               <Popover.Panel
                 as="ul"
-                className="bg-white text-slate-900 ring-slate-900/5 absolute inset-x-0 top-full mt-4 origin-top space-y-4 rounded-2xl p-6 text-lg tracking-tight shadow-xl ring-1"
+                className="absolute inset-x-0 top-full mt-4 origin-top space-y-4 rounded-2xl bg-white p-6 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
               >
                 <li>
                   <Link href="#features">
@@ -81,7 +81,7 @@ function MobileNavigation() {
                     </div>
                   </Link>
                 </li>
-                <li className="border-slate-300/40 border-t pt-4">
+                <li className="border-t border-slate-300/40 pt-4">
                   <Link href="/login">
                     <div className="block w-full">Sign in</div>
                   </Link>
@@ -103,36 +103,36 @@ const Header = () => {
           <ul className="flex items-center">
             <li>
               <Link href="#">
-                <div className="">
+                <div className="bg-blue-300">
                   <span className="sr-only">Home</span>
-                  <Logo className="h-10 w-auto" />
+                  <Logo className="h-10 w-auto " />
                 </div>
               </Link>
             </li>
             <li className="ml-12 hidden md:block">
               <Link href="#features">
-                <div className="text-slate-700 hover:bg-slate-100 hover:text-slate-900 rounded-lg py-1 px-2">
+                <div className="rounded-lg py-1 px-2 text-slate-700 hover:bg-slate-100 hover:text-slate-900">
                   Features
                 </div>
               </Link>
             </li>
             <li className="ml-6 hidden md:block">
               <Link href="#testimonials">
-                <div className="text-slate-700 hover:bg-slate-100 hover:text-slate-900 rounded-lg py-1 px-2">
+                <div className="rounded-lg py-1 px-2 text-slate-700 hover:bg-slate-100 hover:text-slate-900">
                   Testimonials
                 </div>
               </Link>
             </li>
             <li className="ml-6 hidden md:block">
               <Link href="#pricing">
-                <div className="text-slate-700 hover:bg-slate-100 hover:text-slate-900 rounded-lg py-1 px-2">
+                <div className="rounded-lg py-1 px-2 text-slate-700 hover:bg-slate-100 hover:text-slate-900">
                   Pricing
                 </div>
               </Link>
             </li>
             <li className="ml-auto hidden md:block">
               <Link href="/login">
-                <div className="text-slate-700 hover:bg-slate-100 hover:text-slate-900 rounded-lg py-1 px-2">
+                <div className="rounded-lg py-1 px-2 text-slate-700 hover:bg-slate-100 hover:text-slate-900">
                   Sign in
                 </div>
               </Link>
