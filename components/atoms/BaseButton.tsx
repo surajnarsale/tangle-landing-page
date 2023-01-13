a6d6307b9dad2be40d5cabba70468db48db2243b
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-01-13 22:50:11 +0530

    fix: fix base button class

--- components/atoms/BaseButton.tsx
+++ components/atoms/BaseButton.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { PropsWithChildren } from 'react'
+import clsx from 'clsx'
 
 type ButtonProps = {
   className?: string
@@ -9,7 +10,12 @@ type ButtonProps = {
 export function BaseButton(props: PropsWithChildren<ButtonProps>): JSX.Element {
   return (
     <Link href={props.href!}>
-      <div className="rounded-3xl bg-dark-500 px-4 py-2 text-light sm:px-7 sm:py-3 ">
+      <div
+        className={clsx(
+          'rounded-3xl bg-dark-500 px-4 py-2 text-light sm:px-7 sm:py-3 ',
+          props.className
+        )}
+      >
         {props.children}
       </div>
     </Link>
