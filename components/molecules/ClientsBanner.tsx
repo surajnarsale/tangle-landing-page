3176c99ae5fe687159e0f2ad804a7fefbd9f672b
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-01-14 11:21:04 +0530

    feat: add clients banner

--- components/molecules/ClientsBanner.tsx
+++ components/molecules/ClientsBanner.tsx
@@ -1,6 +1,7 @@
 import type { PropsWithChildren } from 'react'
 import clsx from 'clsx'
 import { Container } from '@/components/atoms/Container'
+import BrandLogo from '@/components/atoms/svgs/BrandLogo.svg'
 
 type ClientsBannerProps = {
   className?: string
@@ -8,7 +9,7 @@ type ClientsBannerProps = {
 
 const ClientsBanner = (props: PropsWithChildren<ClientsBannerProps>): JSX.Element => {
   return (
-    <div className={clsx('bg-dark-300 pt-16', props.className)}>
+    <div className={clsx('bg-dark-300 py-16', props.className)}>
       <Container className="mx-auto max-w-[1110px] px-5 pb-5 text-dark-500 ">
         <div className={clsx('', props.className)}>
           <p className="font-melodrama text-4xl font-bold text-primary-100">
@@ -17,6 +18,15 @@ const ClientsBanner = (props: PropsWithChildren<ClientsBannerProps>): JSX.Elemen
               30+ brands globally.
             </span>
           </p>
+        </div>
+        <div className=" mt-14 md:mt-20  md:flex md:justify-between -md:grid -md:grid-cols-2 -md:gap-8 ">
+          {[1, 2, 3, 4, 5].map(() => {
+            return (
+              <>
+                <BrandLogo className="grid h-[40px] w-[100px]  " />
+              </>
+            )
+          })}
         </div>
       </Container>
     </div>
