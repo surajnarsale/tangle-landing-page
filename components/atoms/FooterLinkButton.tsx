afe4f5bd9ca359300609f3ca326b0c233b8300b8
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-01-14 13:05:06 +0530

    feat: add social links card

---
+++ components/atoms/FooterLinkButton.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from 'react'
+import clsx from 'clsx'
+import Link from 'next/link'
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid'
+
+type FooterLinkButtonProps = {
+  className?: string
+  href?: string
+}
+
+export function FooterLinkButton(props: PropsWithChildren<FooterLinkButtonProps>): JSX.Element {
+  return (
+    <Link href={props.href!}>
+      <div
+        className={clsx(
+          'flex max-w-[266px] items-center justify-between  rounded-3xl bg-dark-500  py-4 px-8 px-4 py-2 text-2xl text-sm text-light sm:px-7 sm:py-3 ',
+          props.className
+        )}
+      >
+        {props.children}
+        <ArrowTopRightOnSquareIcon className="h-8 w-8" aria-hidden="true" />
+      </div>
+    </Link>
+  )
+}
--- components/molecules/SocialLinks.tsx
+++ components/molecules/SocialLinks.tsx
@@ -1,13 +1,56 @@
 import type { PropsWithChildren } from 'react'
 import clsx from 'clsx'
 import { Container } from '@/components/atoms/Container'
+import { FooterLinkButton } from '@/components/atoms/FooterLinkButton'
+
+const links = [
+  { name: 'Services' },
+  { name: 'Privacy Policy' },
+  { name: 'Work' },
+  { name: 'Cookies' },
+  { name: 'About us' },
+  { name: 'Career' },
+]
+const socialLinks = [
+  { name: 'Services' },
+  { name: 'Privacy Policy' },
+  { name: 'Work' },
+  { name: 'Cookies' },
+  { name: 'About us' },
+  { name: 'Career' },
+]
 
 type SocialLinksProps = {
   className?: string
 }
 
 const SocialLinks = (props: PropsWithChildren<SocialLinksProps>): JSX.Element => {
-  return <div>Social Links</div>
+  return (
+    <div className={clsx('bg-primary-200 py-10', props.className)}>
+      <Container className="mx-auto max-w-[1110px] px-5 pb-5 text-dark-500 ">
+        <div className="-md:grid -md:grid-cols-2 -md:gap-4">
+          {links.map((link) => {
+            return (
+              <>
+                <p className="text-xl font-medium ">{link.name}</p>
+              </>
+            )
+          })}
+        </div>
+        <div className="mt-10">
+          {links.map((link) => {
+            return (
+              <>
+                <FooterLinkButton className="mt-6" href="#">
+                  {link.name}
+                </FooterLinkButton>
+              </>
+            )
+          })}
+        </div>
+      </Container>
+    </div>
+  )
 }
 
 export default SocialLinks
