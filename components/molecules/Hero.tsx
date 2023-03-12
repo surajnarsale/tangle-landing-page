112ed28b1ce26e6d1431521bab7ce88aa6491ed3
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-03-13 02:41:18 +0530

    feat: add text slide up animation

--- components/molecules/Hero.tsx
+++ components/molecules/Hero.tsx
@@ -5,6 +5,7 @@ import Balancer from 'react-wrap-balancer'
 import { motion } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 import { useEffect } from 'react'
+import AnimatedText from '@/components/molecules/AnimateText'
 
 const Hero = () => {
   // const [ref, inView] = useInView()
@@ -17,17 +18,60 @@ const Hero = () => {
   //   console.log('heroright?', inView)
   // })
 
+  const Heading = [
+    { type: 'heading1', text: 'Bringing beauty &' },
+    {
+      type: 'heading1',
+      text: 'functionality to your',
+    },
+    {
+      type: 'heading1',
+      text: 'digital products.',
+      class:
+        'relative font-generalSans font-medium italic underline decoration-orange decoration-4',
+    },
+  ]
+  const caption = [{ type: 'paragraph', text: 'from India to the world' }]
+  const subheading = [
+    {
+      type: 'paragraph',
+      text: 'At Tangle Design Studio, we help brands go beyond design by solving real digital problems through our data-driven research methods.',
+    },
+  ]
+
+  const container = {
+    visible: {
+      transition: {
+        staggerChildren: 0.025,
+      },
+    },
+  }
+
   return (
     <Container className=" mx-auto max-w-[1110px] px-5 pb-5 pt-20 text-dark-500 lg:pt-32 ">
-      <motion.p
+      {/* <motion.p
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.2, ease: 'easeInOut' }}
-        className="mx-auto mb-3  text-sm sm:text-lg"
+        
       >
         from India to the world
-      </motion.p>
-      <motion.h1
+      </motion.p> */}
+
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={container}
+        className="mx-auto mb-3  text-sm sm:text-lg"
+      >
+        <div className="container">
+          {caption.map((item, index) => {
+            return <AnimatedText {...item} key={index} />
+          })}
+        </div>
+      </motion.div>
+
+      {/* <motion.h1
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.2, ease: 'easeInOut' }}
@@ -40,7 +84,20 @@ const Hero = () => {
             digital products.
           </span>
         </span>
-      </motion.h1>
+      </motion.h1> */}
+
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={container}
+        className=" mx-auto  pb-5 font-melodrama text-4xl font-bold text-slate-900 sm:text-7xl"
+      >
+        <div className="container">
+          {Heading.map((item, index) => {
+            return <AnimatedText {...item} key={index} className={item?.class} />
+          })}
+        </div>
+      </motion.div>
       <div className=" mt-12 flex flex-row md:space-x-14 -sm:flex-col   ">
         <motion.div
           // ref={ref1}
@@ -58,7 +115,7 @@ const Hero = () => {
             placeholder="blur"
           />
         </motion.div>
-        <motion.div
+        {/* <motion.div
           // ref={ref2}
           initial={{ visibility: 'hidden' }}
           animate={{ visibility: 'visible' }}
@@ -72,6 +129,19 @@ const Hero = () => {
               data-driven research methods.
             </span>
           </Balancer>
+        </motion.div> */}
+
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={container}
+          className=" h-full text-2xl md:self-end -sm:pt-6 -sm:pb-16 -sm:text-xl "
+        >
+          <div className="container">
+            {subheading.map((item, index) => {
+              return <AnimatedText {...item} key={index} />
+            })}
+          </div>
         </motion.div>
       </div>
     </Container>
---
+++ pages/dist/blog.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var Home_1 = require("@/components/Blog/Home");
+var LandingPage_1 = require("@/components/templates/LandingPage");
+function Home() {
+    return (React.createElement("div", { className: "max-w-screen" },
+        React.createElement(LandingPage_1["default"], null,
+            React.createElement(Home_1["default"], null))));
+}
+exports["default"] = Home;
